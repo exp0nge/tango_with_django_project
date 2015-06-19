5539e0cc8a41005dbec37e6cb8513f88bf020235
exp0nge <mdislamwork@gmail.com>
2015-06-18 19:29:27 -0600

    Initial commit

---
+++ tango_with_django_project/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls import include, url, patterns
+from django.contrib import admin
+from django.conf import settings
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'tango_with_django_project.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^rango/', include('rango.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls'))
+]
+
+if settings.DEBUG:
+    urlpatterns += patterns('django.views.static',
+                            (r'^media/(?P<path>.*)',
+                             'serve',
+                             {'document_root': settings.MEDIA_ROOT}),
+                            )
